25428e59106aa22f08baf14f504b5f0667a433fd
KolyaPronin <kolya.pronin.0506@mail.ru>
2026-04-21 00:17:10 +0700

    added tests

--- src/test/java/com/kolya/TaskTimeTracker/integration/task/TaskPersistenceIT.java
+++
@@ -1,4 +0,0 @@
-package com.kolya.TaskTimeTracker.integration.task;
-
-public class TaskPersistenceIT {
-}
--- src/test/java/com/kolya/TaskTimeTracker/integration/timerecord/TimeRecordPersistenceIT.java
+++
@@ -1,4 +0,0 @@
-package com.kolya.TaskTimeTracker.integration.timerecord;
-
-public class TimeRecordPersistenceIT {
-}
